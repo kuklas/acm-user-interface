0a2f8956bff9d4f5cd676a5aab981c39c174fe56
kuklas <skukla@redhat.com>
2025-10-10 16:39:12 +0200

    Updates

--- src/app/navigation/infrastructure/ClusterDetailPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ClusterDetail } from '@app/Clusters/ClusterDetail';
-
-export const ClusterDetailPage: React.FunctionComponent = () => {
-  return <ClusterDetail />;
-};
